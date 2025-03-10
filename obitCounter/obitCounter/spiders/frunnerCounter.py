ef739a996619be710214cf10ca25fc0b089dd1a6
Coulter Ulrich <coulterulrich0@gmail.com>
2025-03-10 16:58:51 -0600

    Started on frunnerCounter.py

--- obitCounter/obitCounter/spiders/frunnerCounter.py
+++ obitCounter/obitCounter/spiders/frunnerCounter.py
@@ -13,4 +13,13 @@ class f1ObitCounter(scrapy.Spider):
     )
 
     name = 'frunnerCounter'
-    urls = ['']
+    urls = ['https://www.swedberg-taylor.com/']
+
+    #Testing site URL: https://www.swedberg-taylor.com/
+    #Obituary URL: https://obituaries.frontrunnerpro.com/runtime/311039/ims/WF2/public/get-records-additional.php
+    #payload: pageNum=1&rpp=6&width=159.6&height=186.78555&type=all&template=below&getServiceType=shiva&sort=dod&guid=MzA3OTA4Ok1haW5TaXRl&wholeSite=true
+    """
+    headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
+        }
+        """
